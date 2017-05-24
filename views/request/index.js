8dcb160d299f902d4c089e2c00581a6be547af45
zouli <394534913@qq.com>
2017-05-24 18:41:37 +0800

    fix

--- views/request/index.js
+++ views/request/index.js
@@ -49,7 +49,6 @@ const install = (Vue) => {
       //用户的请求uuid,唯一标识
       const uuid = store.state.user.user_info !== null ? '/' + store.state.user.user_info.uuid : ''
       config.url = base + uuid + config.url
-      console.log(config.url)
       //在发送请求之前做某事
       $vue.$NProgress.start()
       return config
