034512a358c11144e6ef0441d791148a6425ab08
zouli <394534913@qq.com>
2017-05-25 15:46:54 +0800

    fix

--- views/router/index.js
+++ views/router/index.js
@@ -8,42 +8,43 @@ import { request_login, result_code } from 'common/request_api'
 
 Vue.use(VueRouter)
 //使用AMD方式 懒加载
-// component: resolve => require(['pages/home'], resolve),
+// component: resolve => require(['pages/home'),
+// () => import('../../component/historyTab/historyTab.vue')
 const routes = [{
   path: '/', name: 'home',
   meta: { title: "主页" },
-  components: { default: resolve => require(['pages/home'], resolve) }
+  components: { default: () => import('pages/home') }
 },
 
   // 用户管理路由   开始
   {
     path: '/user/base', name: 'userBase',
     meta: { title: "用户列表" },
-    components: { default: resolve => require(['pages/user/base'], resolve) },
+    components: { default: () => import('pages/user/base') },
   }, {
     path: '/user/update/:id', name: 'userUpdate',
     meta: { title: "用户修改" },
-    components: { default: resolve => require(['pages/user/save'], resolve) },
+    components: { default: () => import('pages/user/save') },
   }, {
     path: '/user/add', name: 'userAdd',
     meta: { title: "添加用户" },
-    components: { default: resolve => require(['pages/user/save'], resolve) },
+    components: { default: () => import('pages/user/save') },
   }, {
     path: '/user/changeGroup/:id', name: 'userChangeGroup',
     meta: { title: "换岗" },
-    components: { default: resolve => require(['pages/user/changeGroup'], resolve) },
+    components: { default: () => import('pages/user/changeGroup') },
   }, {
     path: '/user/setHierarchy/:id', name: 'setHierarchy',
     meta: { title: "设置层级" },
-    components: { default: resolve => require(['pages/user/setHierarchy'], resolve) },
+    components: { default: () => import('pages/user/setHierarchy') },
   }, {
     path: '/user/disableRole/:id', name: 'disableRole',
     meta: { title: "角色禁用" },
-    components: { default: resolve => require(['pages/user/disableRole'], resolve) },
+    components: { default: () => import('pages/user/disableRole') },
   }, {
     path: '/user/disablePrivilege/:id', name: 'disablePrivilege',
     meta: { title: "权限禁用" },
-    components: { default: resolve => require(['pages/user/disablePrivilege'], resolve) },
+    components: { default: () => import('pages/user/disablePrivilege') },
   },
   // 用户管理路由   结束
 
@@ -51,24 +52,24 @@ const routes = [{
   {
     path: '/privilege/base', name: 'privilegeBase',
     meta: { title: "权限列表" },
-    components: { default: resolve => require(['pages/privilege/base'], resolve) },
+    components: { default: () => import('pages/privilege/base') },
   }, {
     path: '/privilege/update/:id', name: 'privilegeUpdate',
     meta: { title: "权限修改" },
-    components: { default: resolve => require(['pages/privilege/save'], resolve) },
+    components: { default: () => import('pages/privilege/save') },
   }, {
     path: '/privilege/add', name: 'privilegeAdd',
     meta: { title: "添加权限" },
-    components: { default: resolve => require(['pages/privilege/save'], resolve) },
+    components: { default: () => import('pages/privilege/save') },
   }, {
     path: '/privilege/setProcess/:id', name: 'setPrivilegeProcess',
     meta: { title: "设置审核流程" },
-    components: { default: resolve => require(['pages/privilege/setProcess'], resolve) },
+    components: { default: () => import('pages/privilege/setProcess') },
   },
   {
     path: '/privilege/setOwner/:id', name: 'setPrivilegeOwner',
     meta: { title: "设置权限owner" },
-    components: { default: resolve => require(['pages/privilege/setOwner'], resolve) },
+    components: { default: () => import('pages/privilege/setOwner') },
   },
   // 权限管理路由   结束
 
@@ -76,29 +77,29 @@ const routes = [{
   {
     path: '/role/base', name: 'roleBase',
     meta: { title: "角色列表" },
-    components: { default: resolve => require(['pages/role/base'], resolve) },
+    components: { default: () => import('pages/role/base') },
   }, {
     path: '/role/update/:id', name: 'roleUpdate',
     meta: { title: "角色修改" },
-    components: { default: resolve => require(['pages/role/save'], resolve) },
+    components: { default: () => import('pages/role/save') },
   }, {
     path: '/role/add', name: 'roleAdd',
     meta: { title: "添加角色" },
-    components: { default: resolve => require(['pages/role/save'], resolve) },
+    components: { default: () => import('pages/role/save') },
   }, {
     path: '/role/setProcess/:id', name: 'setRoleProcess',
-    meta: {title: "设置审核流程"},
-    components: {default: resolve => require(['pages/role/setProcess'], resolve)},
+    meta: { title: "设置审核流程" },
+    components: { default: () => import('pages/role/setProcess') },
   },
   {
     path: '/role/setOwner/:id', name: 'setRoleOwner',
     meta: { title: "设置角色owner" },
-    components: { default: resolve => require(['pages/role/setOwner'], resolve) },
+    components: { default: () => import('pages/role/setOwner') },
   },
   {
     path: '/role/setRoleHasPrivilege/:id', name: 'setRoleHasPrivilege',
     meta: { title: "设置角色拥有权限" },
-    components: { default: resolve => require(['pages/role/setRoleHasPrivilege'], resolve) },
+    components: { default: () => import('pages/role/setRoleHasPrivilege') },
   },
   // 角色管理路由   结束
 
@@ -106,16 +107,16 @@ const routes = [{
   {
     path: '/hierarchy/base', name: 'hierarchyBase',
     meta: { title: "层级列表" },
-    components: { default: resolve => require(['pages/hierarchy/base'], resolve) },
+    components: { default: () => import('pages/hierarchy/base') },
   }, {
     path: '/hierarchy/add', name: 'hierarchyAdd',
     meta: { title: "添加层级" },
-    components: { default: resolve => require(['pages/hierarchy/save'], resolve) },
+    components: { default: () => import('pages/hierarchy/save') },
   },
   {
     path: '/hierarchy/update/:id', name: 'hierarchyUpdate',
     meta: { title: "修改层级" },
-    components: { default: resolve => require(['pages/hierarchy/save'], resolve) },
+    components: { default: () => import('pages/hierarchy/save') },
   },
   //层级管理路由   结束
 
@@ -123,55 +124,54 @@ const routes = [{
   {
     path: '/business/base', name: 'businessBase',
     meta: { title: "商家列表" },
-    components: { default: resolve => require(['pages/business/base'], resolve) },
+    components: { default: () => import('pages/business/base') },
   }, {
     path: '/business/add', name: 'businessAdd',
     meta: { title: "添加商家" },
-    components: { default: resolve => require(['pages/business/save'], resolve) },
+    components: { default: () => import('pages/business/save') },
   },
   {
     path: '/business/update/:id', name: 'businessUpdate',
     meta: { title: "修改商家" },
-    components: { default: resolve => require(['pages/business/save'], resolve) },
+    components: { default: () => import('pages/business/save') },
   }, {
     path: '/business/setBusinessHasPrivilege/:id', name: 'setBusinessHasPrivilege',
-    meta: {title: "设置商家权限"},
-    components: {default: resolve => require(['pages/business/setBusinessHasPrivilege'], resolve)},
+    meta: { title: "设置商家权限" },
+    components: { default: () => import('pages/business/setBusinessHasPrivilege') },
   }, {
     path: '/business/setBusinessHasRole/:id', name: 'setBusinessHasRole',
-    meta: {title: "导入商家权限"},
-    components: {default: resolve => require(['pages/business/setBusinessHasRole'], resolve)},
+    meta: { title: "导入商家权限" },
+    components: { default: () => import('pages/business/setBusinessHasRole') },
   },
 
   // 商家管理路由   结束
-
 
   // 部门管理路由   开始
   {
     path: '/group/base', name: 'groupBase',
     meta: { title: "部门列表" },
-    components: { default: resolve => require(['pages/group/base'], resolve) },
+    components: { default: () => import('pages/group/base') },
   }, {
     path: '/group/add', name: 'groupAdd',
     meta: { title: "添加部门" },
-    components: { default: resolve => require(['pages/group/save'], resolve) },
+    components: { default: () => import('pages/group/save') },
   },
   {
     path: '/group/update/:id', name: 'groupUpdate',
     meta: { title: "修改部门" },
-    components: { default: resolve => require(['pages/group/save'], resolve) },
+    components: { default: () => import('pages/group/save') },
   }, {
     path: '/group/groupHasUser/:id', name: 'groupHasUser',
     meta: { title: "设置部门成员" },
-    components: { default: resolve => require(['pages/group/groupHasUser'], resolve) },
+    components: { default: () => import('pages/group/groupHasUser') },
   }, {
     path: '/group/groupHasLeader/:id', name: 'groupHasLeader',
     meta: { title: "设置部门Leader" },
-    components: { default: resolve => require(['pages/group/groupHasLeader'], resolve) },
+    components: { default: () => import('pages/group/groupHasLeader') },
   }, {
     path: '/group/groupHasRole/:id', name: 'groupHasRole',
     meta: { title: "设置部门角色" },
-    components: { default: resolve => require(['pages/group/groupHasRole'], resolve) },
+    components: { default: () => import('pages/group/groupHasRole') },
   },
   // 部门管理路由   结束
 
@@ -179,40 +179,39 @@ const routes = [{
   {
     path: '/searchPrivilege/base', name: 'searchPrivilegeBase',
     meta: { title: "基本表格" },
-    components: { default: resolve => require(['pages/searchPrivilege/base'], resolve) },
+    components: { default: () => import('pages/searchPrivilege/base') },
   }, {
     path: '/searchPrivilege/add', name: 'searchPrivilegeAdd',
     meta: { title: "数据修改" },
-    components: { default: resolve => require(['pages/searchPrivilege/save'], resolve) },
+    components: { default: () => import('pages/searchPrivilege/save') },
   },
   // 权限查询路由  结束
-
 
   // 审核流程管理路由   开始
   {
     path: '/process/base', name: 'processBase',
     meta: { title: "审核流程列表" },
-    components: { default: resolve => require(['pages/process/base'], resolve) },
+    components: { default: () => import('pages/process/base') },
   }, {
     path: '/process/add', name: 'processAdd',
-    meta: {title: "添加审核流程"},
-    components: {default: resolve => require(['pages/process/save'], resolve)},
+    meta: { title: "添加审核流程" },
+    components: { default: () => import('pages/process/save') },
   }, {
     path: '/process/update/:id', name: 'processUpdate',
-    meta: {title: "修改审核流程"},
-    components: {default: resolve => require(['pages/process/save'], resolve)},
+    meta: { title: "修改审核流程" },
+    components: { default: () => import('pages/process/save') },
   }, {
     path: '/process/lowRisk', name: 'lowRisk',
-    meta: {title: "低风险模板"},
-    components: {default: resolve => require(['pages/process/lowRisk'], resolve)},
-  },{
+    meta: { title: "低风险模板" },
+    components: { default: () => import('pages/process/lowRisk') },
+  }, {
     path: '/process/middleRisk', name: 'middleRisk',
-    meta: {title: "中风险模板"},
-    components: {default: resolve => require(['pages/process/middleRisk'], resolve)},
-  },{
+    meta: { title: "中风险模板" },
+    components: { default: () => import('pages/process/middleRisk') },
+  }, {
     path: '/process/highRisk', name: 'highRisk',
-    meta: {title: "高风险模板"},
-    components: {default: resolve => require(['pages/process/highRisk'], resolve)},
+    meta: { title: "高风险模板" },
+    components: { default: () => import('pages/process/highRisk') },
   },
   // 审核流程管理路由   结束
 
@@ -220,19 +219,19 @@ const routes = [{
   {
     path: '/approval/base', name: 'approvalBase',
     meta: { title: "审核列表" },
-    components: { default: resolve => require(['pages/approval/base'], resolve) },
+    components: { default: () => import('pages/approval/base') },
   }, {
     path: '/approval/agentEdit', name: 'agentEdit',
     meta: { title: "设置代签" },
-    components: { default: resolve => require(['pages/approval/agentEdit'], resolve) },
+    components: { default: () => import('pages/approval/agentEdit') },
   }, {
     path: '/approval/changeSign/:id', name: 'changeSign',
     meta: { title: "转签" },
-    components: { default: resolve => require(['pages/approval/changeSign'], resolve) },
+    components: { default: () => import('pages/approval/changeSign') },
   }, {
     path: '/approval/addSign/:id', name: 'addSign',
     meta: { title: "加签" },
-    components: { default: resolve => require(['pages/approval/addSign'], resolve) },
+    components: { default: () => import('pages/approval/addSign') },
   },
   // 审核管理路由   结束
 
@@ -240,11 +239,11 @@ const routes = [{
   {
     path: '/userAndRole/base', name: 'userAndRoleBase',
     meta: { title: "用户角色授权列表" },
-    components: { default: resolve => require(['pages/userAndRole/base'], resolve) },
+    components: { default: () => import('pages/userAndRole/base') },
   }, {
     path: '/userAndRole/userRoleEdit/:id', name: 'userRoleEdit',
     meta: { title: "用户角色授权" },
-    components: { default: resolve => require(['pages/userAndRole/userRoleEdit'], resolve) },
+    components: { default: () => import('pages/userAndRole/userRoleEdit') },
   },
   // 用户角色授权管理路由   结束
 
@@ -252,15 +251,15 @@ const routes = [{
   {
     path: '/apply/base', name: 'applyBase',
     meta: { title: "申请列表" },
-    components: { default: resolve => require(['pages/apply/base'], resolve) },
+    components: { default: () => import('pages/apply/base') },
   }, {
     path: '/apply/add', name: 'applyAdd',
     meta: { title: "权限申请" },
-    components: { default: resolve => require(['pages/apply/privilegeSave'], resolve) },
+    components: { default: () => import('pages/apply/privilegeSave') },
   }, {
     path: '/apply/add', name: 'applyRoleAdd',
     meta: { title: "角色申请" },
-    components: { default: resolve => require(['pages/apply/roleSave'], resolve) },
+    components: { default: () => import('pages/apply/roleSave') },
   },
   //申请管理路由 结束
 
@@ -268,15 +267,15 @@ const routes = [{
   {
     path: '/monitor/base', name: 'monitorBase',
     meta: { title: "监控配置管理列表" },
-    components: { default: resolve => require(['pages/monitor/base'], resolve) },
-  },{
+    components: { default: () => import('pages/monitor/base') },
+  }, {
     path: '/monitor/add', name: 'monitorAdd',
     meta: { title: "添加监控配置" },
-    components: { default: resolve => require(['pages/monitor/save'], resolve) },
-  },{
+    components: { default: () => import('pages/monitor/save') },
+  }, {
     path: '/monitor/update/:id', name: 'monitorUpdate',
     meta: { title: "修改监控配置" },
-    components: { default: resolve => require(['pages/monitor/save'], resolve) },
+    components: { default: () => import('pages/monitor/save') },
   },
 
   //监控配置管理路由 结束
@@ -285,29 +284,28 @@ const routes = [{
   {
     path: '/monitorResult/base', name: 'monitorResultBase',
     meta: { title: "监控结果列表" },
-    components: { default: resolve => require(['pages/monitorResult/base'], resolve) },
+    components: { default: () => import('pages/monitorResult/base') },
   },
   //监控结果管理路由 结束
-
 
   {
     path: '/table/base', name: 'tableBase',
     meta: { title: "基本表格" },
-    components: { default: resolve => require(['pages/table/base'], resolve) },
+    components: { default: () => import('pages/table/base') },
   }, {
     path: '/table/update/:id', name: 'tableUpdate',
     meta: { title: "数据修改" },
-    components: { default: resolve => require(['pages/table/save'], resolve) },
+    components: { default: () => import('pages/table/save') },
   }, {
     path: '/table/add', name: 'tableAdd',
     meta: { title: "添加数据" },
-    components: { default: resolve => require(['pages/table/save'], resolve) },
+    components: { default: () => import('pages/table/save') },
   }, {
     path: '/500', name: '500',
-    components: { fullView: resolve => require(['pages/error/500'], resolve) }
+    components: { fullView: () => import('pages/error/500') }
   }, {
     path: '*', name: 'notPage',
-    components: { fullView: resolve => require(['pages/error/404'], resolve) }
+    components: { fullView: () => import('pages/error/404') }
   },
 
 ]
